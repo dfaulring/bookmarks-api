4755c0fbfd3fce67cc850af4d4636a8048e4d433
Dominic Faulring <dfaulring@gmail.com>
2022-08-07 10:33:26 -0700

    added modules

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common'
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { message: 'Signed up successfully' }
+  }
+
+  signin() {
+    return { message: 'Signed in successfully' }
+  }
+}
